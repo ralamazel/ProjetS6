5c1120f6153be5454421b294a49ab52704bfd61b
igouane <igouane@mbp-de-maxime-2>
2019-01-16 18:23:17 +0100

    pour stock

--- Code/src/Chaine.java
+++ Code/src/Chaine.java
@@ -3,5 +3,10 @@ public abstract class Chaine {
 	protected QuantiteElement[] input;
 	protected QuantiteElement[] output;
 	protected int max;
+	
+	
+	//abstract void fabriquer() {
+		
+	}
 
-}
+
--- Code/src/Stock.java
+++ Code/src/Stock.java
@@ -1,4 +1,13 @@
 
 public class Stock {
-
+	private QuantiteElement[] stock;
+	private int max;
+	public Stock() {
+		this.max=100;
+		this.stock=new QuantiteElement[max];
+		
+	//	this.stock[0]= new QuantitePlaqueAlu()
+	}
+	
+//	public
 }
